141495e26c45e4b8bea6b2a79ccf6cbfc25e45c8
yanjie <lee87902407@yeah.net>
2026-04-07 19:53:26 +0800

    定义统一的 Buffer 接口与类型标识
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- mempool/interface.go
+++ mempool/interface.go
@@ -1,10 +1,37 @@
 package mempool
 
+import "unsafe"
+
+type BufferType uint8
+
+const (
+	BufferTypeHeap BufferType = iota
+	BufferTypeNative
+)
+
+type FreeFunc func(unsafe.Pointer)
+
 type BytePool interface {
 	Get(size int) []byte
 	Put(buf []byte)
 	Bucket(size int) int
 	MaxPooledCap() int
+}
+
+type Buffer interface {
+	Type() BufferType
+	Bytes() []byte
+	Len() int
+	Cap() int
+	Released() bool
+	Reset()
+	Clone() []byte
+	EnsureCapacity(additional int)
+	Resize(n int)
+	Append(p []byte)
+	AppendByte(v byte)
+	DetachedCopy() []byte
+	Release()
 }
 
 type bufferLifecycle interface {
